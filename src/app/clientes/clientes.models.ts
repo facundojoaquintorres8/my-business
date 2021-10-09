86c4bbd95c51da74e278beb5279a89aa6ee169a9
pablo-nardi <nardipablo31@gmail.com>
2021-10-08 23:43:18 -0300

    abm de clientes

---
+++ src/app/clientes/clientes.models.ts
@@ -0,0 +1,9 @@
+export interface ICliente{
+    dni: string;
+    nombre: string;
+    apellido: string;
+    telefono: string;
+    direccion: string;
+    esMayorista: boolean;
+    activo: boolean;
+}
